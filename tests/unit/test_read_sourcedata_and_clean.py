e6701f556b4911b4d3cf7caa98e644312aa9a747
jlplenio <ubtown@gmail.com>
2020-07-17 15:15:27 +0200

    multiprocessing test fixtures

--- tests/unit/test_read_sourcedata_and_clean.py
+++ tests/unit/test_read_sourcedata_and_clean.py
@@ -2,19 +2,25 @@
 import pytest
 from bovheat_src import bh_input, bovheat
 
+# runs tests with multiprocessing. 0: auto-core, 1: disabled, 2: fixed 2 cores
+@pytest.fixture(params=[0, 1, 2])
+def cpu_count(request):
+    return request.param
+
+
 # Test pass
 @pytest.mark.parametrize(
     "lang, rel_path, out1, out2",
     [
         # Test 1 - eng and xlsx
-        ("eng", "tests/unit/test_read_sourcedata_and_clean/Test1_eng_xlsx/", 6994, 6204,),
+        ("eng", "tests/unit/test_read_sourcedata_and_clean/Test1_eng_xlsx/", 6994, 6204),
         # Test 2 - ger and xls
         ("ger", "tests/unit/test_read_sourcedata_and_clean/Test2_ger_xls/", 6970, 6366),
     ],
 )
-def test_calc_calving_date_pass(lang, rel_path, out1, out2):
+def test_calc_calving_date_pass(lang, cpu_count, rel_path, out1, out2):
     # Test reading files
-    df = bh_input.get_source_data(lang, relative_path=rel_path)
+    df = bh_input.get_source_data(lang, cpu_count, relative_path=rel_path)
     assert len(df) == out1
 
     # Test cleaning loaded data
@@ -39,10 +45,10 @@ def test_calc_calving_date_pass(lang, rel_path, out1, out2):
         ),
     ],
 )
-def test_calc_calving_date_fail(lang, rel_path, exception_msg):
+def test_calc_calving_date_fail(lang, cpu_count, rel_path, exception_msg):
     # Expect Exception
     with pytest.raises(Exception) as e:
-        assert bh_input.get_source_data(lang, relative_path=rel_path)
+        assert bh_input.get_source_data(lang, cpu_count, relative_path=rel_path)
 
     # Check exception message
     assert exception_msg in str(e.value)
